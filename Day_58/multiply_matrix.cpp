f442a72ed73f707ff5abab9477c4908f2dbf5348
Sneha Tiwari <sneha224tiwari@gmail.com>
2025-07-30 23:36:05 +0530

    day_58

---
+++ Day_58/multiply_matrix.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+  public:
+    bool multiplyMatrix(int mat1[4][4], int mat2[4][4], int result[4][4]) {
+        int res[4][4] = {0};
+        
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                for (int k = 0; k < 4; ++k) {
+                    res[i][j] += mat1[i][k] * mat2[k][j];
+                }
+            }
+        }
+        
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                if (res[i][j] != result[i][j]) return false;
+            }
+        }
+        
+        return true;
+    }
+};
